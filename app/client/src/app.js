106c1e48f0dba1af1ec763cf1e84ad0ef1b30814
mawashicaprio <tanishkgupta88@gmail.com>
2017-07-25 22:48:52 +0530

    listing router added

--- app/client/src/app.js
+++ app/client/src/app.js
@@ -24,3 +24,31 @@ app
 
   }]);
 
+
+  var app = angular.module('list', [
+  'ui.router',
+]);
+
+app
+  .config([
+    '$httpProvider',
+    function($httpProvider){
+
+      // Add auth token to Authorization header
+      $httpProvider.interceptors.push('AuthInterceptor');
+
+    }])
+  .run([
+    'AuthService',
+    'Session',
+    function(AuthService, Session){
+
+      // Startup, login if there's  a token.
+      var token = Session.getToken();
+      if (token){
+        AuthService.loginWithToken(token);
+      }
+
+  }]);
+
+
---
+++ app/client/views/listing/listingCtrl.js
@@ -0,0 +1,16 @@
+angular.module('reg')
+  .controller('ListingCtrl', [
+    '$rootScope',
+    '$scope',
+    '$sce',
+    'currentUser',
+    'settings',
+    'Utils',
+    'AuthService',
+    'UserService',
+    'EVENT_INFO',
+    'DASHBOARD',
+    function($rootScope, $scope, $sce, currentUser, settings, Utils, AuthService, UserService, DASHBOARD){
+     
+
+    }]);
