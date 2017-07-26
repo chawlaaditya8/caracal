589b291e44b826d2acb4a163282551d67de6276f
Aditya Chawla <chawlaaditya8@gmail.com>
2017-07-26 23:57:44 +0530

    listing service added again :P

---
+++ app/client/src/services/ListingService.js
@@ -0,0 +1,28 @@
+angular.module('reg')
+  .factory('ListingService', [
+  '$http',
+  'Session',
+  function($http, Session){
+
+    var base = '/api/';
+
+    return {
+
+      // ----------------------
+      // Basic Actions
+      // ----------------------
+      // getCurrentUser: function(){
+      //   return $http.get(base + Session.getUserId());
+      // },
+
+      // get: function(id){
+      //   return $http.get(base + id);
+      // },
+
+      getAll: function(){
+        return $http.get(base + 'events');
+      },
+
+    };
+  }
+  ]);
