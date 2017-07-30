c5a4c476d9f9ca1a157dcea7420d3474ec89fb5a
Aditya Chawla <chawlaaditya8@gmail.com>
2017-07-30 21:37:10 +0530

    ui of listing and individual event modified

--- app/client/views/listing/listingCtrl.js
+++ app/client/views/listing/listingCtrl.js
@@ -11,10 +11,24 @@ angular.module('reg')
     'DASHBOARD',
     function($rootScope, $scope, $sce, currentUser, settings, Utils, ListingService, DASHBOARD){
 
+      // Semantic-UI moves modal content into a dimmer at the top level.
+      // While this is usually nice, it means that with our routing will generate
+      // multiple modals if you change state. Kill the top level dimmer node on initial load
+      // to prevent this.
+      $('.ui.dimmer').remove();
+      // Populate the size of the modal for when it appears, with an arbitrary user.
+      $scope.selectedEvent = {};
+      $scope.event = 
       ListingService
         .getAll()
         .success(function(data){
           $scope.events = data;
         });
-
+      $scope.selectEvent = function(event){
+        console.log("test");
+        console.log(event);
+        $scope.selectedEvent = event;
+        $('.event.modal')
+          .modal('show');
+      }
     }]);
